5237c1305d0f799592b8e09513c60052c8dccc30
Gian <58370608+glpecile@users.noreply.github.com>
2024-02-19 12:16:28 -0300

    feat: add auto light/dark mode support

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={cn(inter.className)}>
-				{children}
-			</body>
+		<html lang="en" suppressHydrationWarning>
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+				disableTransitionOnChange
+			>
+				<body className={cn(inter.className)}>
+					{children}
+				</body>
+			</ThemeProvider>
 		</html>
 	);
 }
