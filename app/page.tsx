87823a6972594f3b7ab733318397fb81171b99df
Gian <58370608+glpecile@users.noreply.github.com>
2024-12-09 09:32:37 -0300

    feat(page): update landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,36 @@
+import { cn } from "@/lib/utils";
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-evenly p-24">
-			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-				<p className="fixed top-0 left-0 flex w-full justify-center border-gray-300 border-b bg-gradient-to-b from-zinc-200 pt-8 pb-6 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
-					Get started by editing&nbsp;
-					<code className="font-bold font-mono">app/page.tsx</code>
+		<main className="flex min-h-screen max-w-full grow flex-col items-center justify-center gap-6 font-mono">
+			<h1 className={cn("font-bold text-xl")}>
+				Welcome to next-shadcn-template
+			</h1>
+			<section className={cn("max-w-2xl space-y-2")}>
+				<p>
+					This is a Next.js template with a few features to help you
+					get started quickly. It includes TypeScript, Biome, Tailwind
+					CSS, and Motion (fka Framer Motion).
 				</p>
-				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
-					<a
-						className="flex place-items-center gap-2 p-8 hover:underline"
-						href="https://glpecile.xyz"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						By Gian Luca Pecile
-					</a>
+				<div className="z-10 w-full items-center justify-between text-sm lg:flex">
+					<p className="fixed top-0 left-0 flex w-full justify-center border-gray-300 border-b bg-gradient-to-b from-zinc-200 pt-8 pb-6 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
+						Get started by editing&nbsp;
+						<code className="font-bold font-mono">
+							app/page.tsx
+						</code>
+					</p>
+					<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
+						<a
+							className="flex place-items-center gap-2 p-8 hover:underline"
+							href="https://glpecile.xyz"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							By Gian Luca Pecile
+						</a>
+					</div>
 				</div>
-			</div>
-
-			<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-				<a
-					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={"mb-3 font-semibold text-2xl"}>
-						Docs{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={"m-0 max-w-[30ch] text-sm opacity-50"}>
-						Find in-depth information about Next.js features and
-						API.
-					</p>
-				</a>
-
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={"mb-3 font-semibold text-2xl"}>
-						Learn{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={"m-0 max-w-[30ch] text-sm opacity-50"}>
-						Learn about Next.js in an interactive course
-						with&nbsp;quizzes!
-					</p>
-				</a>
-
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={"mb-3 font-semibold text-2xl"}>
-						Templates{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={"m-0 max-w-[30ch] text-sm opacity-50"}>
-						Explore starter templates for Next.js.
-					</p>
-				</a>
-
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={"mb-3 font-semibold text-2xl"}>
-						Deploy{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p
-						className={
-							"m-0 max-w-[30ch] text-balance text-sm opacity-50"
-						}
-					>
-						Instantly deploy your Next.js site to a shareable URL
-						with Vercel.
-					</p>
-				</a>
-			</div>
+			</section>
 		</main>
 	);
 }
